ceeb58ca85c4064a067fe2239dfc76eb552ad290
Julio Uribe <uribejulioc@gmail.com>
2023-11-10 14:00:18 -0800

    Update nextJS version

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en">
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="purple">
           <NavBar />
