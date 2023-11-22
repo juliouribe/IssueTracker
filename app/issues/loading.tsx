446611080a3b7d93a511be18b4b4ed5b3b70520a
Julio Uribe <uribejulioc@gmail.com>
2023-11-22 14:50:01 -0800

    Refactor app components with index component and organize imports

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,8 +1,6 @@
-import { Table } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import IssuesActions from './issuesActions'
+import { Skeleton } from "@/app/components";
+import { Table } from '@radix-ui/themes';
+import IssuesActions from './issuesActions';
 
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5];
